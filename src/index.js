d80bdb8af99fd78235523a7889404ceacb6b7bbf
Joseph2006t <laizatellojoseph@gmail.com>
2025-01-16 18:57:36 -0500

    cambios menores

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url)); // pega o diretório 
 app.set('views', join(__dirname, 'views')); // especifica o diretório de views 
 app.set('view engine', 'ejs'); // especifica o motor de renderizacion
 app.get('/', (req, res) => res.render('index')); // renderiza a vista index.ejs
-app.use()
+app.use(router)
 app.listen(3000); 
 console.log('Server is running on port 3000'); // mensaje de confirmacion
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
-import { Routes } from "express";
+import { Router } from "express";
 
 const router = Router();
 
 router.get('/', (req, res) => res.render('index',{title:'La Cevicheria El Pulpito'}));
 router.get('/about', (req, res) => res.render('about',{title:'Acerca de nosotros'}));
+router.get('/contacto', (req,res) => res.render('contacto',{title:'Contactanos'}));
+
+router.get('/promociones', (req,res) => res.render('promociones',{title:'Acerca de las Promociones'}));
+
+export default router; // exporta el router para ser utilizado en otros archivos
