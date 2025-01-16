c543502ff02584c5d3c95ca9a7c698adad78ff2c
Joseph2006t <laizatellojoseph@gmail.com>
2025-01-16 17:59:20 -0500

    commit 1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import {dirname, join} from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express(); 
+const __dirname = dirname(fileURLToPath(import.meta.url)); // pega o diretório actual
+app.set('views', join(__dirname, 'views')); // especifica o diretório de views 
+app.set('view engine', 'ejs'); // especifica o motor de renderizacion
+app.get('/', (req, res) => res.render('index')); // renderiza a vista index.ejs
+app.listen(3000); 
+console.log('Server is running on port 3000'); // mensaje de confirmacion
