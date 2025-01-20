a0e6d050c5f6b29d017be75b670679398ae48939
Joseph2006t <laizatellojoseph@gmail.com>
2025-01-20 15:10:19 -0500

    cambios en contacto

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import {dirname, join} from 'path';
 import { fileURLToPath } from 'url';
 import router from './routes/index.js';
 
+
 const app = express(); 
+
+
+
 const __dirname = dirname(fileURLToPath(import.meta.url)); // pega o diretório actual
 app.set('views', join(__dirname, 'views')); // especifica o diretório de views 
 app.set('view engine', 'ejs'); // especifica o motor de renderizacion
@@ -11,3 +15,8 @@ app.get('/', (req, res) => res.render('index')); // renderiza a vista index.ejs
 app.use(router)
 app.listen(3000); 
 console.log('Server is running on port 3000'); // mensaje de confirmacion
+
+
+
+
+
